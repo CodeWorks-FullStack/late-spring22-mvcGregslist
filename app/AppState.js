d71676141f26d8b2ceef025353f59a1f196fefe7
Mick Shannahan <MickShanny@gmail.com>
2022-05-31 14:46:13 -0600

    added async

--- app/AppState.js
+++ app/AppState.js
@@ -1,4 +1,3 @@
-import { Car } from "./Models/Car.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
@@ -6,10 +5,7 @@ class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
   values = []
   /** @type {import('./Models/Car').Car[]} */
-  cars = [
-    new Car({make: 'Chevrolet', model: 'Impala', description:'It balla', price: 10, year: 1964, color: '#000', imgUrl: 'https://m.media-amazon.com/images/I/51IpgyJ3GmL._AC_SX466_.jpg'}),
-    new Car({make: 'Buick', model: 'Grand National', description: 'It is grand', price:15, year: 1984, color:'#ff0000', imgUrl:'http://www.deansgarage.com/wp-content/uploads/GNpropsals.jpg'})
-  ]
+  cars = []
   houses = []
 }
 
--- app/Models/Car.js
+++ app/Models/Car.js
@@ -4,7 +4,7 @@ import { generateId } from "../Utils/generateId.js"
 export class Car{
   constructor(carData){
     // NOTE need id to have something unique on each car
-    this.id = generateId()
+    this.id = carData.id || generateId()
     this.make = carData.make
     this.model = carData.model
     this.description = carData.description
@@ -18,13 +18,14 @@ export class Car{
   get Template(){
     return `
   <div class=" col-6 col-md-3">
-    <div class="rounded shadow p-2" ">
+    <div class="rounded shadow bg-white p-2" ">
       <img class="img-fluid" src="${this.imgUrl}" alt="">
       <h5 class="text-center">${this.make} | ${this.model} | ${this.year}</h5>
       <h4 class="text-center">$${this.price}</h4>
       <p>${this.description}</p>
       <input class="w-100" type="color" value="${this.color}">
       <button class="btn btn-danger" onclick="app.carsController.deleteCar('${this.id}')"><i class="mdi mdi-delete"></i></button>
+      <button class="btn btn-warning" onclick="app.carsController.openEditForm('${this.id}')"><i class="mdi mdi-pencil"></i></button>
     </div>
   </div>
     `
